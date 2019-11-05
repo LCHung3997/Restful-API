2c1fb55df97fee5629c99907d98bb33d9774c536
lehung22297 <44271319+lehung22297@users.noreply.github.com>
2019-11-05 11:12:06 +0700

    Api (nodejs) cho  game caro

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// var usersRouter = require('./routes/users');
+// var user = require('./routes/users');
+const auth = require('./routes/auth');
+require('./middleware/passport');
 var app = express();
 
 // view engine setup
@@ -14,17 +17,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(passport.initialize());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "*");
+  next(); 
+});
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', auth);
+// app.use('/user', passport.authenticate('jwt', {session: false}), user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
--- model/index.model.js
+++ model/index.model.js
@@ -4,4 +4,20 @@ module.exports = {
   load: () => {
     return db.load('select * from Account');
   },
+  addAccount: account => {
+    return db.add('Account', account);
+  },
+  getUser: user =>{
+    return db.load(`select * from Account where Username = '${user}' `);
+  },
+  getGmail: gmail =>{
+    return db.load(`select * from Account where gmail = '${gmail}' `);
+  },
+  findOneById: id => {
+    return db.load(`select * from Account where ID = '${id}' `);
+  },
+  update: entity => {
+    return db.update('Account', 'ID', entity);
+  },
+
 };
